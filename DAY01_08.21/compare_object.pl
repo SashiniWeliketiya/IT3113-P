85031a8802a57de95b98b2042f60dc0c1d3b2e2d
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2025-08-24 21:15:25 +0530

    Delete DAY01_08.21 directory

--- DAY01_08.21/compare_object.pl
+++
@@ -1,8 +0,0 @@
-likes(john,ann).
-studies(charlie,ai).
-teaches(collins,cs).
-loves(vincent,mia).
-is_the_next_month_of(october,september).
-is_a_student_of(jane,roy).
-color(elephant,black).
-
--- DAY01_08.21/exercise.pl
+++
@@ -1,6 +0,0 @@
-parent_of(tom,bob).
-parent_of(pam,bob).
-parent_of(tom,liz).
-parent_of(bob,ann).
-parent_of(bob,pat).
-parent_of(pat,jim).
